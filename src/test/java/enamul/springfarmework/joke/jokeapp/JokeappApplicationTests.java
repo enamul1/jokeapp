b32032f4eddaa8795192496c85fdac951e061735
Enamul <enamul.promy@gmail.com>
2019-12-26 16:47:24 +0600

    initial project setup

---
+++ src/test/java/enamul/springfarmework/joke/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package enamul.springfarmework.joke.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
